2f7bf8bf4b39d9b865548b4523d096af6d1280af
Isabella Morini <imorini@Isabellas-MacBook-Pro-2.local>
2018-03-21 18:58:07 -0400

    Added entity framework

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fisher.Bookstore.Api.Models
 {
@@ -6,5 +7,8 @@ namespace Fisher.Bookstore.Api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Bio { get; set; }
+        public List<Book> Titles { get; set; }
+
     }
 }
